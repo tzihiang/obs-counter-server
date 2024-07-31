401738c1e3182cd872bd4e1c232eeaa214431964
Willis Tay <willistzh1996@hotmail.sg>
2024-07-31 21:41:30 +0800

    Updated server.js to extend the number of life counters

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # obs-counter-server
 Server code of OBS Counter
 
-Need to create 2 files on your own:
+Create a folder at the root node called "lifeCounts". Inside them, add as many files as you would like, but name them in this format
 
-value1.txt
-value2.txt
+value<id>.txt
+
+`id` will be used as the reference in this case. 
 
 Both should have 40 as the start. Link those files to OBS
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
+
 const app = express();
-
-const FILE_PATH_1 = 'value1.txt';
-const FILE_PATH_2 = 'value2.txt';
-
 app.use(bodyParser.json());
+
+const lifeValueDir = path.join(__dirname, 'lifeCOunts');
 
 // Add your origin URLs here. This includes the IPv4 of the machine that is hosting the UI server and its respective port
 const allowedOrigins = ['http://localhost:3000'];
@@ -32,41 +32,17 @@ const writeValue = (filePath, value) => {
     fs.writeFileSync(filePath, value.toString());
 };
 
-app.get('/value1', (req, res) => {
-    const value = readValue(FILE_PATH_1);
-    res.json({ value });
+app.get('/life/:id', (req, res) => {
+    const filePath = path.join(lifeValueDir, `value${req.params.id}.txt`);
+    const currValue = readValue(filePath);
+    res.send({ value: currValue })
 });
 
-app.post('/value1', (req, res) => {
-    const action = req.body.action;
-    let value = readValue(FILE_PATH_1);
-    if (action === 'increment') {
-        value += 1;
-    } else if (action === 'decrement') {
-        value -= 1;
-    }
-
-    writeValue(FILE_PATH_1, value);
-    res.json({ value });
-});
-
-app.get('/value2', (req, res) => {
-    const value = readValue(FILE_PATH_2);
-    res.json({ value });
-});
-
-app.post('/value2', (req, res) => {
-    const action = req.body.action;
-    let value = readValue(FILE_PATH_2);
-    if (action === 'increment') {
-        value += 1;
-    }
-    else if (action === 'decrement') {
-        value -= 1;
-    }
-
-    writeValue(FILE_PATH_2, value);
-    res.json({ value });
+app.post('/life/:id', (req, res) => {
+    const filePath = path.join(lifeValueDir, `value${req.params.id}.txt`);
+    const newValue = req.body.value;
+    writeValue(filePath, newValue);
+    res.send(`Value Updated to ${newValue}`)
 });
 
 const PORT = process.env.PORT || 5000;
